b6c4aa634a70c4d46a497371f0b5cf0023f71266
abhinav <avnvbhatta@gmail.com>
2020-06-23 11:04:54 -0400

    basic ticketmaster calls

--- src/components/mapview/mapview.js
+++ src/components/mapview/mapview.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import "./mapview.scss";
 import { getCurrentPosition } from "../../utils/utils";
-
+import axios from "axios";
   
 const MapView = () => {  
     const [map, setMap] = useState(null);
@@ -10,6 +10,7 @@ const MapView = () => {
     const [coordinates, setCoordinates] = useState({
         longitude: -80.9981576, latitude: 35.045021999999996
     });
+    const [events, setEvents] = useState(null);
 
     useEffect(() => {    
         const fetchCoordinates = async () => {
@@ -22,9 +23,49 @@ const MapView = () => {
                 console.error(error);
             }
         };
+
+        const getTicketMasterEvents = async () => {
+          console.log('ticketmaster')
+          const response = await axios.get('https://app.ticketmaster.com/discovery/v2/events.json', {
+              params: {
+                apikey: "LMX62etGfMGogQNGUjnr9Dh2yvtQ88sI",
+                geoPoint: Math.round(coordinates.latitude)+","+Math.round(coordinates.longitude)
+              }
+            });
+            //const data = await response.json();
+            const events = response.data._embedded.events;
+            console.log(events);
+
+            let eventsArray = []
+            events.map(event => {
+              let eventInfo = {
+                name: event.name,
+                date: {
+                  startTime: event.dates.start.localTime,
+                  startDate: event.dates.start.localDate
+                },
+                images: event.images,
+                url: event.url,
+                venue: event._embedded.venues[0].name,
+                city: event._embedded.venues[0].city,
+                state: event._embedded.venues[0].state,
+                location: event._embedded.venues[0].location,
+                parking: event._embedded.venues[0].parkingDetail,
+                priceRange: event.priceRanges
+              }
+              eventsArray.push(eventInfo);
+            })
+            setEvents(eventsArray)
+            console.log(events)
+            
+        }
+
+        getTicketMasterEvents();
+
       mapboxgl.accessToken = "pk.eyJ1IjoiYXZudmJoYXR0YSIsImEiOiJja2JyOXRjancxNGthMndsbTE4dDNrdDB0In0.-CLS7_4DVmpgROSDPOoltA";
 
       const initializeMap = ({ setMap, mapContainer }) => {
+        console.log('initialize map')
         const map = new mapboxgl.Map({
           container: mapContainer.current,
           style: "mapbox://styles/mapbox/streets-v11", // stylesheet location
@@ -39,6 +80,7 @@ const MapView = () => {
 
         //For creating popups
         // create a HTML element for each popup
+        console.log(events, 'from inside map')
         var el = document.createElement('div');
         el.className = 'marker';
 
@@ -53,7 +95,7 @@ const MapView = () => {
       };
   
       if (!map) initializeMap({ setMap, mapContainer });
-    });
+    }, []);
     
     return <div ref={el => (mapContainer.current = el)} className="mapContainer" />;
   };
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,9 @@
+
+//function to get geo location of user
 export function getCurrentPosition() {
     return new Promise((resolve, reject) => {
         navigator.geolocation.getCurrentPosition(resolve, reject);
     });
 }
+
+
