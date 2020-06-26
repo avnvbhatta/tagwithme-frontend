f8a12e2b39959250da088ca1d77df52ca15b33e0
abhinav <avnvbhatta@gmail.com>
2020-06-26 10:28:45 -0400

    added vibrant code

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,4 @@
+import * as Vibrant from 'node-vibrant'
 
 //function to get geo location of user
 export function getCurrentPosition() {
@@ -6,4 +7,18 @@ export function getCurrentPosition() {
     });
 }
 
-
+export function getVibrantColors(imgPath){
+        return Vibrant.from(imgPath).getPalette() 
+        .then((palette) => {
+            let result = {
+                vibrant: palette.Vibrant.hex,
+                darkVibrant: palette.DarkVibrant.hex,
+                lightVibrant: palette.LightVibrant.hex,
+                muted: palette.Muted.hex,
+                darkMuted: palette.DarkMuted.hex,
+                lightMuted: palette.LightMuted.hex,
+            }
+            return result;
+        })
+        .catch(err => console.error('An error occured: ', err))
+}
