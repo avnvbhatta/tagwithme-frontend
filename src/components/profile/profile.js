02089de66ae7d0e115fc8ce161d59e58cc51624f
abhinav <avnvbhatta@gmail.com>
2020-06-26 10:28:38 -0400

    basic profile section

---
+++ src/components/profile/profile.js
@@ -0,0 +1,38 @@
+import React from "react"
+import "./profile.scss"
+import * as Vibrant from 'node-vibrant'
+import { getVibrantColors } from "../../utils/utils";
+import { FacebookOutlined, TwitterOutlined, InstagramOutlined, AimOutlined } from '@ant-design/icons';
+
+const Profile = (props) => {
+    return ( 
+        <div>
+            <div className="profileContainer">
+                <div className="userInfo">
+                    <div className="userImg" >
+                        <img  src={"./abhinav.png"} alt=""/>
+                    </div>
+                    <div className="userDetails">
+                        <div className="userName">Binish Koirala</div>
+                        <div className="userLocation">
+                            <AimOutlined />
+                            <div>Des Moines, IA</div>
+                            
+                        </div>
+                        <div className="userSocial">
+                            <FacebookOutlined className="userSocialIcons"/>
+                            <TwitterOutlined className="userSocialIcons"/>
+                            <InstagramOutlined className="userSocialIcons"/>
+                        </div>
+                        <div className="messageUser">
+                            <button>Message</button>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    );
+}
+ 
+export default Profile;
