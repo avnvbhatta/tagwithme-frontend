4cfae4c56b4a7f1d98a055320357bc2199c525e2
abhinav <avnvbhatta@gmail.com>
2020-06-30 23:56:54 -0400

    Added context to store user

--- src/App.js
+++ src/App.js
@@ -11,45 +11,56 @@ import { Layout } from 'antd';
 import Sidebar from './components/sidebar/sidebar';
 import Messages from './components/messages/messages';
 import Notifications from './components/notifications/notifications';
+import { UserContext } from './utils/usercontext';
+import { useState } from 'react';
+import { useMemo } from 'react';
+
 
 const { Sider,Content } = Layout;
 
 
 function App() {
+  //Creating a context, or a global store to hold our user details.
+  const [user, setUser] = useState(null);
+  //will only change the providedUser value if the user is changed
+  const providedUser = useMemo(() => ({ user, setUser }), [user, setUser]);
+
+
   return (
     <div className="App">
-      {/* <Sidebar /> */}
-
       <BrowserRouter>
         <Layout>
           <Sidebar>Sider</Sidebar>
           <Layout>
             <Content>
               <Switch>
-                <Route path="/" exact>
-                  <LoginForm />
-                </Route>
-                <Route path="/login">
-                  <LoginForm />
-                </Route>
-                <Route path="/signup">
-                  <SignUpForm />
-                </Route>
-                <Route path="/profile">
-                  <Profile />
-                </Route>
-                <Route path="/home">
-                  <EventsView />
-                </Route>
-                <Route path="/events">
-                  <EventsView />
-                </Route>
-                <Route path="/messages">
-                  <Messages />
-                </Route>
-                <Route path="/notifications">
-                  <Notifications />
-                </Route>
+              <UserContext.Provider value={providedUser}>
+                  <Route path="/" exact>
+                    <LoginForm />
+                  </Route>
+                  <Route path="/login">
+                    <LoginForm />
+                  </Route>
+                  <Route path="/signup">
+                    <SignUpForm />
+                  </Route>
+                  <Route path="/profile">
+                    <Profile />
+                  </Route>
+                  <Route path="/home">
+                    <EventsView />
+                  </Route>
+                  <Route path="/events">
+                    <EventsView />
+                  </Route>
+                  <Route path="/messages">
+                    <Messages />
+                  </Route>
+                  <Route path="/notifications">
+                    <Notifications />
+                  </Route>
+                </UserContext.Provider>
+                
               </Switch>
             </Content>
           </Layout>
