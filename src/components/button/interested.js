36ef0ab17900c1781daecd1f2ab03066ea80dd01
abhatta <avnvbhatta@gmail.com>
2020-07-14 12:26:04 -0400

    replaced antd button with html button

--- src/components/button/interested.js
+++ src/components/button/interested.js
@@ -1,29 +1,57 @@
-import React, {useContext} from 'react';
+import React, {useState} from 'react';
 import "./interested.scss";
 import { Button } from 'antd';
-import { StarOutlined } from '@ant-design/icons';
+import { StarOutlined, StarFilled } from '@ant-design/icons';
 import {connect} from "react-redux";
 import axiosForAPI from "../../utils/axiosForAPI"
 
 
 const InterestedButton = (props) => {
-    const event = props.event;
+    const [isInterested, setIsInterested] = useState(props.event.isInterested);
     const handleClick = async () =>{
-        const data = {
-            ...event,
-            userId: props.userData.id
-        }
         
-        let res = await axiosForAPI.post(process.env.REACT_APP_API_CREATE_INTERESTED_EVENT_ENDPOINT, data);
-        console.log(res);
+        if(isInterested){
+            const data = {
+                eventId: props.event.id,
+                userId: props.userData.id
+            }
+            try{
+                let res = await axiosForAPI.delete(process.env.REACT_APP_API_CREATE_INTERESTED_EVENT_ENDPOINT, { data: data });
+                //update the store
+                props.updateEvent(props.event.id)
+            }
+            catch(error){
+                console.log(error)
+            }
+            
+        }
+        else{
+            const data = {
+                ...props.event,
+                userId: props.userData.id
+            }
+            try {
+                let res = await axiosForAPI.post(process.env.REACT_APP_API_CREATE_INTERESTED_EVENT_ENDPOINT, data);
+                //update the store
+                props.updateEvent(props.event.id)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        
+
+        //update local event state
+        setIsInterested(!isInterested);
+        
 
     }
     return ( 
-        <Button type="primary" shape="round" icon={<StarOutlined />} size={'large'}
+        <button className="interestedBtn"
         onClick={handleClick}
         >
-          Interested
-        </Button>
+          {isInterested ? <StarFilled /> : <StarOutlined />} Interested
+        </button>
     );
 }
 
@@ -32,5 +60,11 @@ const mapStateToProps = (state) =>{
         userData: state.userData   
     }
 }
+
+const mapDispatchToProps = (dispatch) =>{
+    return{
+        updateEvent: (eventId) => dispatch({type: 'UPDATE_EVENT_INTERESTED', val: eventId})
+    }
+}
  
-export default connect(mapStateToProps)(InterestedButton);
+export default connect(mapStateToProps, mapDispatchToProps)(InterestedButton);
