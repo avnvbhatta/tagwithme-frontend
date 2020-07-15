815c26259b26e23233246368fa2addf9880e71d9
abhatta <avnvbhatta@gmail.com>
2020-07-14 23:31:41 -0400

    changed to flat db style object

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,26 +8,23 @@ export const filterTicketMasterEvents = (events, interestedEvents) => {
         //Only store required details from ticketmaster response
         let eventInfo = {
             id: event.id || "bad_id",
-            isInterested: interestedEvents.has(event.id) ? true : false,
+            isinterested: interestedEvents.has(event.id) ? true : false,
             name: event.name || "Untitled Event",
-            classification: event.classifications === undefined ? [] : {
-                segment: event.classifications[0].segment.name,
-                genre: event.classifications[0].genre.name
-            },
-            date: {
-                startTime: event.dates.start.localTime === undefined ? "" : moment(event.dates.start.localTime, 'HH:mm:ss').format("hA"),
-                startDate: event.dates.start.localDate === undefined ? "" : moment(event.dates.start.localDate).format("ddd, MMM Do, YYYY"),
-            },
-            images: event.images || [],
+            segment: event.classifications[0].segment.name || "",
+            genre: event.classifications[0].genre.name || "",
+            startTime: moment(event.dates.start.localTime, 'HH:mm:ss').format("hA") || "",
+            startDate: moment(event.dates.start.localDate).format("ddd, MMM Do, YYYY")  || "",
+            images: event.images[0].url || "",
             url: event.url || "",
             venue: event._embedded.venues[0].name || "",
-            distance: event._embedded.venues[0].distance === undefined ? "" : `${event._embedded.venues[0].distance} miles away`,
+            distance: `${event._embedded.venues[0].distance} miles away` || "",
             address: event._embedded.venues[0].address.line1 || "",
-            city: event._embedded.venues[0].city || "",
-            state: event._embedded.venues[0].state || {},
-            location: event._embedded.venues[0].location || { latitude: 40.730610, longitude: -73.935242 },
+            city: event._embedded.venues[0].city.name || "",
+            state: event._embedded.venues[0].state.stateCode || "",
+            lat: event._embedded.venues[0].location.latitude || "", //{ latitude: 40.730610, longitude: -73.935242 },
+            lng: event._embedded.venues[0].location.longitude || "", //{ latitude: 40.730610, longitude: -73.935242 },
             parking: event._embedded.venues[0].parkingDetail || "",
-            priceRange: event.priceRanges === undefined ? "" : `$${event.priceRanges[0].min} - $${event.priceRanges[0].max}`,
+            priceRange: `$${event.priceRanges[0].min} - $${event.priceRanges[0].max}` || "",
             postalCode: event._embedded.venues[0].postalCode || ""
         }
         eventsArray.push(eventInfo);
