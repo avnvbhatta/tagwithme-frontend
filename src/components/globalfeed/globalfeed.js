66d9464c023515caafc7d363b8f20cafe7538950
abhatta <avnvbhatta@gmail.com>
2020-07-31 16:13:48 -0400

    working likes and comments

--- src/components/globalfeed/globalfeed.js
+++ src/components/globalfeed/globalfeed.js
@@ -1,6 +1,6 @@
 import React, { createElement, useEffect, useState } from 'react';
 import axiosForAPI from "../../utils/axiosForAPI";
-import { Select, Space, Comment, Tooltip, Avatar  } from 'antd';
+import { Select, Space, Comment, Tooltip, Avatar, Input  } from 'antd';
 import moment from 'moment';
 import { LoadingOutlined, DislikeOutlined, LikeOutlined, DislikeFilled, LikeFilled, UserOutlined } from '@ant-design/icons';
 import {connect} from "react-redux";
@@ -12,6 +12,8 @@ import { useHistory, Link } from "react-router-dom";
 
 
 const { Option } = Select;
+const { TextArea } = Input;
+
 
 const GlobalFeed = (props) => {
     const [radius, setRadius] = useState('5');
@@ -24,39 +26,52 @@ const GlobalFeed = (props) => {
     }
 
     const [likes, setLikes] = useState(0);
-    const [dislikes, setDislikes] = useState(0);
     const [action, setAction] = useState(null);
+    const [showCommentInput, setShowCommentInput] = useState(null);
+    const [comment, setComment] = useState(null);
+    const [addingComment, setAddingComment] = useState(false);
+    
 
-    const like = () => {
-        setLikes(1);
-        setDislikes(0);
-        setAction('liked');
+    const like = (event) => {
+        let data = {
+            liker_id: props.userId,
+            user_id: event.userid,
+            event_id: event.id,
+            increment:  event.like_user_id.includes(props.userId)  ? false : true
+        }
+        axiosForAPI.put(process.env.REACT_APP_UPDATE_LIKES_ENDPOINT, data).then(res => {
+        })
     };
 
-    const dislike = () => {
-        setLikes(0);
-        setDislikes(1);
-        setAction('disliked');
-    };
+    const toggleCommentInput = (event) => {
+        if(showCommentInput){
+            setShowCommentInput(null);
+        }
+        else{
+            setShowCommentInput(event)
+        }
+    }
 
-    const actions = [
-        <Tooltip key="comment-basic-like" title="Like">
-        <span onClick={like}>
-            {createElement(action === 'liked' ? LikeFilled : LikeOutlined)}
-            <span className="comment-action">{likes}</span>
-        </span>
-        </Tooltip>,
-        <Tooltip key="comment-basic-dislike" title="Dislike">
-        <span onClick={dislike}>
-            {React.createElement(action === 'disliked' ? DislikeFilled : DislikeOutlined)}
-            <span className="comment-action">{dislikes}</span>
-        </span>
-        </Tooltip>,
-        <span key="comment-basic-reply-to">Reply to</span>,
-
+    const addComment = () =>{
+        setAddingComment(true);
+        const data = {
+            user_id: showCommentInput.userid,
+            event_id: showCommentInput.id,
+            author_id: props.userId,
+            comment: comment
+        }
         
-    ];
+        axiosForAPI.post(process.env.REACT_APP_ADD_COMMENT_ENDPOINT, data).then(res => {
+            setAddingComment(false);
+        })
 
+    }
+
+    const handleTextAreaChange = (e) => {
+        setComment(e.target.value);
+    }
+
+         
     useEffect(()=>{
         const data = {
             userId: props.userId,
@@ -65,6 +80,7 @@ const GlobalFeed = (props) => {
             radius: radius
         }
         axiosForAPI.post(process.env.REACT_APP_API_GET_GLOBAL_FEED_EVENTS_ENDPOINT, data).then(res => {
+            console.log(res.data)
             setGlobalEvents(res.data);
             setIsLoading(false);
         })
@@ -94,8 +110,19 @@ const GlobalFeed = (props) => {
                 {globalEvents.map(event => {
                     return <Comment
                         key={`${event.timestamp}`}
-                        actions={[ ...actions, <InterestedButton event={event}/>,
-                            <FollowButton event={event} fromGlobal={true}/>]}
+                        actions={
+                            [  
+                                <Tooltip key="comment-basic-like" title="Like">
+                                <span onClick={() => like(event)}>
+                                    {event.like_user_id.includes(props.userId)  ? <LikeFilled style={{color: "dodgerblue"}}/> : <LikeOutlined/>}
+                                    <span className="comment-action">{event.likes}</span>
+                                </span>
+                                </Tooltip>,
+                                <span key="comment-basic-reply-to" onClick={() => toggleCommentInput(event)}>Comment</span>,
+                                <InterestedButton event={event}/>,
+                                <FollowButton event={event} fromGlobal={true}/>
+                            
+                            ]}
                         author={<div><Link to={`/profile/${event.userid}`}>{event.username}</Link></div>}
                         avatar={ 
                         <Avatar
@@ -110,9 +137,12 @@ const GlobalFeed = (props) => {
                                 <p>
                                     Going to {event.name} on {event.startdate} at {event.starttime}. 
                                 </p>
-                                {/* <InterestedButton event={event}/> */}
-                                {/* <FollowButton event={event} fromGlobal={true}/> */}
-
+                                {event === showCommentInput && <div className="comment-area">
+                                    <TextArea rows={1} autoSize allowClear onChange={handleTextAreaChange}/>
+                                    <button className="my-btn" onClick={addComment}>{addingComment ? <LoadingOutlined /> : "Add Comment"}</button>
+                                </div>
+                                }
+                                
                             </div>
                         }
                         datetime={
@@ -120,7 +150,33 @@ const GlobalFeed = (props) => {
                             <span>{moment(event.timestamp).fromNow()}</span>
                         </Tooltip>
                         }
-                    />
+                        
+                    >
+                        {event.comments && event.comments.map(comment => {
+                            return <Comment
+                            key={comment.created_at}
+                                author={<div><Link to={`/profile/${comment.author_id}`}>{comment.author_name}</Link></div>}
+                                avatar={
+                                <Avatar
+                                    src={comment.author_imgurl ? `${process.env.REACT_APP_API_GET_UPLOAD_ENDPOINT}${comment.author_imgurl}` : ""}
+                                    icon={comment.author_imgurl  ? "" :<UserOutlined />}
+                                    alt={comment.author_name}
+                                />
+                                }
+                                content={
+                                <p>
+                                    {comment.comment}
+                                </p>
+                                }
+                                datetime={
+                                <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
+                                    <span>{moment(comment.created_at).fromNow()}</span>
+                                </Tooltip>
+                                }
+                            />
+                                
+                        }) }
+                    </Comment>
 
                 })}
             </div>
